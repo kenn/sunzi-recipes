a333ea617edec3571f9483f0b44f58c8a084bce0
Kenn Ejima <kenn.ejima@gmail.com>
2012-02-28 13:44:32 -0800

    Adding mongodb-10gen.

---
+++ debian/mongodb-10gen.sh
@@ -0,0 +1,15 @@
+# MongoDB
+# http://www.mongodb.org/display/DOCS/Ubuntu+and+Debian+packages
+
+if grep -Fq "mongodb-10gen" /etc/apt/sources.list; then
+  echo 'mongodb-10gen already exists'
+else
+  echo 'deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen' >> /etc/apt/sources.list
+
+  # Install 10gen GPG key
+  apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+
+  # Install MongoDB
+  aptitude update
+  aptitude install mongodb-10gen
+fi
