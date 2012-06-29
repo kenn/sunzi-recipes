2b8f566c946e444d496235af6dd1ac6c3d23a96e
Kenn Ejima <kenn.ejima@gmail.com>
2012-06-28 18:06:18 -0700

    Better this way.

--- debian/sysstat.sh
+++ debian/sysstat.sh
@@ -1,9 +1,9 @@
 # sysstat, system performance tools for Linux. for sar, iostat, etc.
 
-sunzi::silencer "aptitude -y install sysstat"
-if grep -Fq 'ENABLED="true"' /etc/default/sysstat; then
-  echo 'sysstat already enabled'
+if dpkg -s "sysstat"; then
+  echo 'sysstat already installed'
 else
+  aptitude -y install sysstat
   sed -i 's/ENABLED="false"/ENABLED="true"/' /etc/default/sysstat
   /etc/init.d/sysstat restart
 fi
