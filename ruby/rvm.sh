25d4205892869f398f86265fb733885b1bcec7c3
Kenn Ejima <kenn.ejima@gmail.com>
2013-03-06 17:19:37 -0800

    Fixing.

--- ruby/rvm.sh
+++ ruby/rvm.sh
@@ -2,9 +2,11 @@
 
 if [ -d /usr/local/rvm ]; then
   echo 'RVM already installed'
-  rvm get latest
 else
   curl -L https://get.rvm.io | bash -s stable --ruby
   echo '[[ -s /usr/local/rvm/scripts/rvm ]] && source /usr/local/rvm/scripts/rvm' >> ~/.bash_profile
 fi
 source ~/.bash_profile
+
+# Updating RVM
+rvm get latest
