76e96b7a801f8ed4250d38874291cacf758dbe66
Kenn Ejima <kenn.ejima@gmail.com>
2013-04-09 15:32:38 -0700

    RVM: do not automatically install rubies.

--- ruby/rvm.sh
+++ ruby/rvm.sh
@@ -3,10 +3,7 @@
 if [ -d /usr/local/rvm ]; then
   echo 'RVM already installed'
 else
-  curl -L https://get.rvm.io | bash -s stable --ruby
+  curl -L https://get.rvm.io | bash -s stable
   echo '[[ -s /usr/local/rvm/scripts/rvm ]] && source /usr/local/rvm/scripts/rvm' >> ~/.bash_profile
 fi
 source ~/.bash_profile
-
-# Updating RVM
-rvm get latest
