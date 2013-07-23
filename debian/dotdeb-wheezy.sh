7036ca18e58051d524d427c51379555e114be68f
Kenn Ejima <kenn.ejima@gmail.com>
2013-07-23 12:16:44 +0900

    Separate dotdeb for squeeze and wheezy.

--- debian/dotdeb-wheezy.sh
+++ debian/dotdeb-wheezy.sh
@@ -1,9 +1,9 @@
-# Dotdeb
+# Dotdeb for Wheezy
 
 if [ -f /etc/apt/sources.list.d/dotdeb.list ]; then
   echo 'dotdeb entry already exists'
 else
   echo 'Adding dotdeb to the apt source list'
-  echo 'deb http://packages.dotdeb.org/ squeeze all' > /etc/apt/sources.list.d/dotdeb.list
+  echo 'deb http://packages.dotdeb.org wheezy all' > /etc/apt/sources.list.d/dotdeb.list
   wget http://www.dotdeb.org/dotdeb.gpg -O - 2> /dev/null | apt-key add -
 fi
