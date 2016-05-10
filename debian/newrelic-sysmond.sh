6990e776654f71e6e01098fae57497cb1758d491
Kenn Ejima <kenn.ejima@gmail.com>
2016-05-10 15:18:25 -0400

    Tweak newrelic-sysmond

--- debian/newrelic-sysmond.sh
+++ debian/newrelic-sysmond.sh
@@ -5,14 +5,14 @@ if dpkg -s "newrelic-sysmond"; then
   echo 'newrelic-sysmond already installed'
 else
   # Add New Relic repository
-  wget -O /etc/apt/sources.list.d/newrelic.list http://download.newrelic.com/debian/newrelic.list
-  wget -O - http://download.newrelic.com/548C16BF.gpg | apt-key add -
+  echo 'deb http://apt.newrelic.com/debian/ newrelic non-free' > /etc/apt/sources.list.d/newrelic.list
+  wget -O - https://download.newrelic.com/548C16BF.gpg | apt-key add -
 
   # Update the catalog, then install the package
-  aptitude update
-  aptitude -y install newrelic-sysmond
+  apt-get update
+  apt-get -y install newrelic-sysmond
 
   # Setup your license key
   nrsysmond-config --set license_key=$1
-  /etc/init.d/newrelic-sysmond restart
+  /etc/init.d/newrelic-sysmond start
 fi
